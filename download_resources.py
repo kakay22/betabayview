312af92d77c2bee1c47bcbe87b08db8e32aeecd0
kakay22 <kylecalites3@gmail.com>
2024-10-25 17:15:58 +0800

    build.sh update

--- download_resources.py
+++ download_resources.py
@@ -1,4 +1,7 @@
 import nltk
 
-# Download the vader_lexicon
-nltk.download('vader_lexicon', download_dir='/opt/render/nltk_data')
+# Download the vader_lexicon if it's not already downloaded
+try:
+    nltk.data.find('sentiment/vader_lexicon.zip')
+except LookupError:
+    nltk.download('vader_lexicon')
