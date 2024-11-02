cc580eee3a34eb75c75f657df6a9c65cc21d866c
kakay22 <kylecalites3@gmail.com>
2024-11-02 18:19:03 +0800

    123

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-export DJANGO_SETTINGS_MODULE=HMS.settings.production
-gunicorn HMS.wsgi:application --bind 0.0.0.0:8000
 
+# Activate the virtual environment
+source venv/bin/activate  # Adjust the path as necessary
+
+# Download the VADER lexicon
 python -m nltk.downloader vader_lexicon
+
+# Set the DJANGO_SETTINGS_MODULE
+export DJANGO_SETTINGS_MODULE=HMS.settings.production
+
+# Start the Django application
+gunicorn HMS.wsgi --log-file -
