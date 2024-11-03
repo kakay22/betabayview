74933a918e689a0d9167c109a1eb9463c79fb67d
kakay22 <kylecalites3@gmail.com>
2024-11-03 20:11:36 +0800

    qwe

--- HMS/settings.py
+++ HMS/settings.py
@@ -68,10 +68,14 @@ INSTALLED_APPS = [
 ]
 
 # Set your AWS credentials and bucket name
+import os
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')  # Default to 'us-east-1' if not set
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 # Set up S3 storage for media files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Boto3Storage'
