e05e872f6b8ed27384a896e9cdd4f3fdf8da8d3d
kakay22 <kylecalites3@gmail.com>
2024-11-03 20:08:16 +0800

    awsds

--- HMS/settings.py
+++ HMS/settings.py
@@ -68,9 +68,10 @@ INSTALLED_APPS = [
 ]
 
 # Set your AWS credentials and bucket name
-AWS_STORAGE_BUCKET_NAME = 'betabayview'
-AWS_S3_REGION_NAME = 'ap-southeast-1'  # e.g., us-east-1
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')  # Default to 'us-east-1' if not set
 
 # Set up S3 storage for media files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Boto3Storage'
