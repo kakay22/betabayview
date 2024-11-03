5d6bbccb62fc487cdec7391677c719adbbcfc3cd
kakay22 <kylecalites3@gmail.com>
2024-11-03 17:23:21 +0800

    hays

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Create a virtual environment if it doesn't exist
-if [ ! -d "venv" ]; then
-    python -m venv venv
-fi
-
 # Activate the virtual environment
 source venv/bin/activate
 
