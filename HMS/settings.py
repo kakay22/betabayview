f6442a8a0d49a6d4c3430fafabf378a5dc9b0a42
kakay22 <kylecalites3@gmail.com>
2024-11-08 09:41:35 +0800

    pic

--- HMS/settings.py
+++ HMS/settings.py
@@ -34,9 +34,6 @@ MEDIA_URL = '/media/'
 ROOT_URLCONF = 'HMS.urls'  # Ensure this points to your urls.py
 
 
-
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -70,15 +67,15 @@ INSTALLED_APPS = [
 # Set your AWS credentials and bucket name
 import os
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')  # Default to 'us-east-1' if not set
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+# AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'us-east-1')  # Default to 'us-east-1' if not set
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = 'public-read'
 
 ASGI_APPLICATION = 'HMS.asgi.application'
 
@@ -105,7 +102,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-WHITENOISE_ROOT = MEDIA_ROOT
+# WHITENOISE_ROOT = MEDIA_ROOT
 
 ROOT_URLCONF = 'HMS.urls'
 
--- HMS/settings/production.py
+++ HMS/settings/production.py
@@ -32,13 +32,9 @@ ROOT_URLCONF = 'HMS.urls'  # Ensure this points to your urls.py
 
 # Set up S3 storage for media files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Boto3Storage'
-# Use S3 for media files in production
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-MEDIA_URL = f'https://betabayview.s3.ap-southeast-1.amazonaws.com/'
+
+# URL path used to access media files
+MEDIA_URL = '/media/'
 
 
 
